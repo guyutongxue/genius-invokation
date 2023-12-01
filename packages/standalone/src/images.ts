b1c9e4ef7853e1d93302eebbfb14ab1fe377f6cc
Guyutongxue <guyutongxue@163.com>
2023-12-02 02:16:22 +0800

    use blob instead of base64

--- packages/standalone/src/images.ts
+++ packages/standalone/src/images.ts
@@ -4,21 +4,19 @@ const imageList: Promise<Record<string, string>> = fetch(
   `/assets/index.json`,
 ).then((r) => r.json());
 
-const cachedImages = new Map<number, Ref<string>>();
+const cachedImages = new Map<number, Ref<string | undefined>>();
 
 export function requestLoad(id: number): Ref<string | undefined> {
   if (!cachedImages.has(id)) {
-    const result = ref();
+    const result = ref<string>();
     cachedImages.set(id, result);
     imageList.then((images) => {
       const path = images[id];
       if (path) {
         fetch(`/assets/${path}.webp`)
-          .then((r) => r.arrayBuffer())
+          .then((r) => r.blob())
           .then((blob) => {
-            result.value =
-              "data:image/webp;base64," +
-              btoa(String.fromCharCode(...new Uint8Array(blob)));
+            result.value = URL.createObjectURL(blob);
           });
       }
     });
