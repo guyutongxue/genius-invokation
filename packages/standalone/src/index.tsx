1f5587dffe754d225ac89eb6ef9165e4dee99871
Guyutongxue <guyutongxue@163.com>
2024-02-08 11:48:41 +0800

    corejs

--- packages/standalone/src/index.tsx
+++ packages/standalone/src/index.tsx
@@ -1,4 +1,5 @@
 /* @refresh reload */
+import "core-js";
 import { render } from "solid-js/web";
 
 import "./index.css";
--- packages/standalone/vite.config.ts
+++ packages/standalone/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
-import { getBabelOutputPlugin } from "@rollup/plugin-babel";
+import babel from "@rollup/plugin-babel";
 
 export default defineConfig({
   plugins: [
     solid(),
-    getBabelOutputPlugin({
-      presets: ["@babel/preset-env"],
-      compact: true,
+    babel({
+      babelHelpers: "bundled",
     }),
   ],
 });
