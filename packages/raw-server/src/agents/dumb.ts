095024013a5ff85f79b9ddfdf0001b0f2f96f6b4
Guyutongxue <guyutongxue@163.com>
2024-04-23 22:09:27 +0800

    add a python example

--- packages/raw-server/src/agents/dumb.ts
+++ packages/raw-server/src/agents/dumb.ts
@@ -1,18 +1,45 @@
-import { ActionRequest, ActionResponse, ChooseActiveRequest, ChooseActiveResponse, NotificationMessage, RerollDiceResponse, SwitchHandsResponse } from "@gi-tcg/typings";
+// Copyright (C) 2024 Guyutongxue
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import {
+  ActionRequest,
+  ActionResponse,
+  ChooseActiveRequest,
+  ChooseActiveResponse,
+  NotificationMessage,
+  RerollDiceResponse,
+  SwitchHandsResponse,
+} from "@gi-tcg/typings";
 import { Agent } from ".";
 
 export class DumbAgent implements Agent {
   async rerollDice(): Promise<RerollDiceResponse> {
-    return { rerollIndexes: [] }; 
+    return { rerollIndexes: [] };
   }
   async switchHands(): Promise<SwitchHandsResponse> {
-    return { removedHands: [] };     
+    return { removedHands: [] };
   }
-  async chooseActive({ candidates }: ChooseActiveRequest): Promise<ChooseActiveResponse> {
-    return { active: candidates[0] };     
+  async chooseActive({
+    candidates,
+  }: ChooseActiveRequest): Promise<ChooseActiveResponse> {
+    return { active: candidates[0] };
   }
   async action({ candidates }: ActionRequest): Promise<ActionResponse> {
-    const declareEndIdx = candidates.findIndex(action => action.type === "declareEnd");
+    const declareEndIdx = candidates.findIndex(
+      (action) => action.type === "declareEnd",
+    );
     return { chosenIndex: declareEndIdx, cost: [] };
   }
 }
--- packages/raw-server/src/agents/index.ts
+++ packages/raw-server/src/agents/index.ts
@@ -1,5 +1,28 @@
+// Copyright (C) 2024 Guyutongxue
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import { PlayerIO } from "@gi-tcg/core";
-import type { ActionRequest, ActionResponse, ChooseActiveRequest, ChooseActiveResponse, NotificationMessage, RerollDiceResponse, SwitchHandsResponse } from "@gi-tcg/typings";
+import type {
+  ActionRequest,
+  ActionResponse,
+  ChooseActiveRequest,
+  ChooseActiveResponse,
+  NotificationMessage,
+  RerollDiceResponse,
+  SwitchHandsResponse,
+} from "@gi-tcg/typings";
 import { DumbAgent } from "./dumb";
 
 export interface Agent {
@@ -36,6 +59,6 @@ export function playerIoFromAgent(agentType: AgentType): PlayerIO {
         case "action":
           return agent.action(data as ActionRequest);
       }
-    }
-  }
+    },
+  };
 }
