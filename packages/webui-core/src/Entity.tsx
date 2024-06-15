317701d70f25a0cfd4ead44ef1b11ec3b9d6b8a1
Guyutongxue <guyutongxue@163.com>
2024-06-15 21:30:13 +0800

    fix 沙王的投影

--- packages/webui-core/src/Entity.tsx
+++ packages/webui-core/src/Entity.tsx
@@ -24,34 +24,36 @@ export interface EntityProps {
 
 export function Summon(props: EntityProps) {
   return (
-    <Interactive
-      class="relative h-15 w-15 rounded-lg"
-      id={props.data.id}
-      definitionId={props.data.definitionId}
-    >
-      <div
-        class="h-full w-full entity absolute top-0 left-0 z-5 rounded-lg"
-        data-highlight={props.data.usagePerRoundHighlight}
-      />
+    <Show when={props.data.definitionId}>
+      <Interactive
+        class="relative h-15 w-15 rounded-lg"
+        id={props.data.id}
+        definitionId={props.data.definitionId}
+      >
+        <div
+          class="h-full w-full entity absolute top-0 left-0 z-5 rounded-lg"
+          data-highlight={props.data.usagePerRoundHighlight}
+        />
         <Image
           imageId={props.data.definitionId}
           class="h-full w-full rounded-lg entity"
         />
-      <Show when={props.data.variable !== null}>
-        <div class="absolute right-0 top-0 bg-white b-1 b-solid b-black w-6 h-6 rounded-3 translate-x-[50%] translate-y-[-50%] flex justify-center items-center z-10">
-          {props.data.variable}
-        </div>
-      </Show>
-      <Show when={props.data.hintIcon !== null}>
-        <div class="absolute h-5 min-w-0 left-0 bottom-0 bg-white bg-opacity-70 flex items-center">
-          <Image
-            imageId={props.data.hintIcon!}
-            class="h-4 w-4 left-0 bottom-0"
-          />
-          {props.data.hintText}
-        </div>
-      </Show>
-    </Interactive>
+        <Show when={props.data.variable !== null}>
+          <div class="absolute right-0 top-0 bg-white b-1 b-solid b-black w-6 h-6 rounded-3 translate-x-[50%] translate-y-[-50%] flex justify-center items-center z-10">
+            {props.data.variable}
+          </div>
+        </Show>
+        <Show when={props.data.hintIcon !== null}>
+          <div class="absolute h-5 min-w-0 left-0 bottom-0 bg-white bg-opacity-70 flex items-center">
+            <Image
+              imageId={props.data.hintIcon!}
+              class="h-4 w-4 left-0 bottom-0"
+            />
+            {props.data.hintText}
+          </div>
+        </Show>
+      </Interactive>
+    </Show>
   );
 }
 
@@ -59,17 +61,19 @@ export { Summon as Support };
 
 export function Status(props: EntityProps) {
   return (
-    <Interactive
-      class="relative h-5 w-5"
-      id={props.data.id}
-      definitionId={props.data.definitionId}
-    >
-      <Image imageId={props.data.definitionId} class="h-5 w-5" />
-      <Show when={props.data.variable !== null}>
-        <div class="absolute bottom-0 right-0 text-xs">
-          {props.data.variable}
-        </div>
-      </Show>
-    </Interactive>
+    <Show when={props.data.definitionId}>
+      <Interactive
+        class="relative h-5 w-5"
+        id={props.data.id}
+        definitionId={props.data.definitionId}
+      >
+        <Image imageId={props.data.definitionId} class="h-5 w-5" />
+        <Show when={props.data.variable !== null}>
+          <div class="absolute bottom-0 right-0 text-xs">
+            {props.data.variable}
+          </div>
+        </Show>
+      </Interactive>
+    </Show>
   );
 }
