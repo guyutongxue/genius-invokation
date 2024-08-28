768455647ff94e463b15a612e2e1a33284b1fe4d
Guyutongxue <guyutongxue@163.com>
2024-08-28 23:28:50 +0800

    bump version

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -23,7 +23,7 @@ import { existsSync } from "node:fs";
 import { PackageJson } from "type-fest";
 
 const packages = ["static-data", "typings", "utils", "core", "data", "webui-core", "webui"];
-const VERSION = "0.9.6";
+const VERSION = "0.10.0";
 
 let doPublish = false;
 if ((await $`which npm`.nothrow().quiet()).exitCode === 0) {
