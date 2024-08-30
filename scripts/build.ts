076d064a54f0664741537269530258dd739a8072
Guyutongxue <guyutongxue@163.com>
2024-08-31 01:30:48 +0800

    fix build order

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,16 +1,16 @@
 #!/usr/bin/env bun
 // Copyright (C) 2024 Guyutongxue
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -22,7 +22,18 @@ import { $ } from "bun";
 
 $.throws(true);
 
-const packages = ["static-data", "typings", "utils", "core", "data", "webui-core", "deck-builder", "detail-log-viewer", "web-client", "standalone"]
+const packages = [
+  "static-data",
+  "typings",
+  "utils",
+  "core",
+  "data",
+  "detail-log-viewer",
+  "webui-core",
+  "deck-builder",
+  "web-client",
+  "standalone",
+];
 
 for (const pkg of packages) {
   await $`bun run build`.cwd(`packages/${pkg}`);
