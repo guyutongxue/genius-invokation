b1ee985a07b676b0ee204883c1b4fe7829fb9bd5
Guyutongxue <guyutongxue@163.com>
2024-10-12 21:47:42 +0800

    use ESNext JSON.parse

--- packages/static-data/scripts/json.ts
+++ packages/static-data/scripts/json.ts
@@ -1,33 +1,27 @@
 // Copyright (C) 2024 Guyutongxue
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { readFileSync } from "node:fs";
-import { isSafeNumber, parse } from "lossless-json";
 
-export function customNumberParser(value: string) {
-  return isSafeNumber(value) ? parseFloat(value) : BigInt(value);
+function reviver(key: string, value: unknown, context?: { source: string }) {
+  return context && typeof value === "number" && !Number.isSafeInteger(value)
+    ? BigInt(context.source)
+    : value;
 }
 
 const cache: Record<string, any> = {};
 export function readJson(path: string) {
-  return (
-    cache[path] ??
-    (cache[path] = parse(
-      readFileSync(path, "utf8"),
-      void 0,
-      customNumberParser,
-    ))
-  );
+  return (cache[path] ??= JSON.parse(readFileSync(path, "utf8"), reviver));
 }
