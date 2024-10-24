fcb52797034166f3eb94410ad118af0b5be35473
Guyutongxue <guyutongxue@163.com>
2024-10-24 17:12:10 +0800

    dynamically import App on entry

--- packages/standalone/src/index.tsx
+++ packages/standalone/src/index.tsx
@@ -14,7 +14,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* @refresh reload */
-import { App } from "./App";
 import { render } from "solid-js/web";
 import { inject } from "@vercel/analytics";
 
@@ -24,6 +23,7 @@ import "./index.css";
   if (import.meta.env.PROD) {
     await import("core-js");
   }
+  const { App } = await import("./App");
   const root = document.getElementById("root")!;
   root.innerHTML = "";
   render(() => <App />, root);
