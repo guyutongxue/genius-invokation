c961b2e47d2581c8045abd712b5237ebfbe0ae68
Guyutongxue <guyutongxue@163.com>
2024-10-24 13:40:29 +0800

    start an experimental test package

---
+++ packages/test/__tests__/basic.test.ts
@@ -0,0 +1,10 @@
+import { setup, _, ref } from "@/main";
+import { Venti } from "@gi-tcg/data/internal/characters/anemo/venti";
+import { Diluc } from "@gi-tcg/data/internal/characters/pyro/diluc";
+
+const target = ref(Diluc);
+
+const ctx = setup({
+  myCharacters: [Venti, _, _],
+  oppCharacters: [target, _, _],
+});
