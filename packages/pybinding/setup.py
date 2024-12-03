feb94d6de77de2930d5fabdc020953e54487d611
Guyutongxue <guyutongxue@163.com>
2024-12-03 19:13:09 +0800

    bump ver

--- packages/pybinding/setup.py
+++ packages/pybinding/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 from setuptools.dist import Distribution
 import json
 from datetime import datetime
+from pathlib import Path
 
 
 class BinaryDistribution(Distribution):
@@ -14,11 +15,17 @@ class BinaryDistribution(Distribution):
 with open("package.json", "r") as f:
     package = json.load(f)
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 # version = f"{package['version']}.dev{datetime.now().strftime('%Y%m%d%H%M')}"
 
 setup(
     name="gitcg",
     version=package['version'],
+    description="A Python binding of Genius-Invokation TCG",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     packages=find_packages(),
     install_requires=["cffi", "protobuf"],
