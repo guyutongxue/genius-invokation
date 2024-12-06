8a8a0184f3c78a27f41414392d2ffc3e8e69268f
Guyutongxue <guyutongxue@163.com>
2024-12-07 01:11:49 +0800

    add csharp binding!

---
+++ packages/csbinding-gen/src/main.rs
@@ -0,0 +1,46 @@
+use clap::Parser;
+use std::env;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[derive(Parser)]
+struct Cli {
+    /// Input C header file
+    input: String,
+    /// Output C# file
+    #[arg(short, long)]
+    output: String,
+    /// DLL name
+    #[arg(short, long)]
+    dll: String,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let temp_filename = format!(
+        "bindgen_{}.rs",
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis()
+    );
+    let temp_path = env::temp_dir().join(temp_filename);
+
+    bindgen::Builder::default()
+        .header(&cli.input)
+        .allowlist_file(&cli.input)
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file(&temp_path)
+        .expect("Unable to write bindings");
+
+    csbindgen::Builder::default()
+        .input_bindgen_file(&temp_path)
+        .csharp_dll_name(&cli.dll)
+        .csharp_namespace("gitcg")
+        .csharp_class_name("NativeMethods")
+        .generate_csharp_file(&cli.output)
+        .expect("Unable to generate C# bindings");
+
+    let _ = std::fs::remove_file(temp_path);
+}
