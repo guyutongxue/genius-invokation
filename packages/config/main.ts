2cbc2667a3a85cd83da19a506709a7ba73cb23cb
Guyutongxue <guyutongxue@163.com>
2024-12-30 11:45:08 +0800

    `web-client` & `server` use beta config (#49)
    * `web-client` & `server` use beta config
    
    * fix build error

--- packages/config/main.ts
+++ packages/config/main.ts
@@ -2,6 +2,11 @@
 export const IS_BETA = false;
 
 export const BETA_VERSION = "v9999.beta";
+
 export const DEFAULT_ASSET_API_ENDPOINT = IS_BETA
   ? `https://beta.assets.gi-tcg.guyutongxue.site/api/v2`
   : `https://assets.gi-tcg.guyutongxue.site/api/v2`;
+  
+export const WEB_CLIENT_BASE_PATH = IS_BETA
+  ? `/gi-tcg-beta`
+  : `/gi-tcg`;
