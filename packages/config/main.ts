15b0601543a43d47f801e5dfea2608101a1ae356
Guyutongxue <guyutongxue@163.com>
2024-12-30 11:03:49 +0800

    Adjust beta asset api endpoint

--- packages/config/main.ts
+++ packages/config/main.ts
@@ -1,3 +1,7 @@
+/** Toggle this when editing beta version. */
 export const IS_BETA = false;
+
 export const BETA_VERSION = "v9999.beta";
-export const DEFAULT_ASSET_API_ENDPOINT = `https://gi-tcg-assets.guyutongxue.site/api/v2`;
+export const DEFAULT_ASSET_API_ENDPOINT = IS_BETA
+  ? `https://beta.assets.gi-tcg.guyutongxue.site/api/v2`
+  : `https://assets.gi-tcg.guyutongxue.site/api/v2`;
