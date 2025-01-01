9974879d27b1e56f8b6f4b6b7935d08bc45941d1
Guyutongxue <guyutongxue@163.com>
2025-01-01 15:55:40 +0800

    bump version (#69)

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -23,7 +23,7 @@ import { existsSync } from "node:fs";
 import { PackageJson } from "type-fest";
 
 const packages = ["static-data", "typings", "utils", "core", "data", "webui-core", "webui"];
-const VERSION = "0.16.1";
+const VERSION = "0.16.2";
 
 const doPublish = !!process.env.PUBLISH;
 if (!doPublish) {
