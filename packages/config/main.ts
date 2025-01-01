ac5d18cf9f6486d6334c72102dfc1b6b439e478c
Guyutongxue <guyutongxue@163.com>
2025-01-01 23:37:14 +0800

    Fix SERVER_HOST default value (#73)
    * web-client, standalone and server customizable host & path
    
    * fix SERVER_HOST default value

--- packages/config/main.ts
+++ packages/config/main.ts
@@ -6,6 +6,8 @@ export const BETA_VERSION = "v9999.beta";
 export const DEFAULT_ASSET_API_ENDPOINT = IS_BETA
   ? `https://beta.assets.gi-tcg.guyutongxue.site/api/v2`
   : `https://assets.gi-tcg.guyutongxue.site/api/v2`;
-  
-export const WEB_CLIENT_BASE_PATH = import.meta.env.WEB_CLIENT_BASE_PATH || '/';
-export const SERVER_HOST = `//${import.meta.env.SERVER_HOST}` || '//localhost:3000';
+
+export const WEB_CLIENT_BASE_PATH = import.meta.env.WEB_CLIENT_BASE_PATH || "/";
+export const SERVER_HOST =
+  `//${import.meta.env.DEV ? "localhost:3000" : import.meta.env.SERVER_HOST}` ||
+  "";
